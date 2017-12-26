0195af227e6bb6d2faf4411f9ab9c3ce2f26cbe6
Mohammed Essaid MEZERREG <moh.essaid@gmail.com>
2017-12-26 08:55:28 +0000

    Add sessions and user id generator

--- api.php
+++ api.php
@@ -1,4 +1,12 @@
 <?php
+$product_id = $_POST['product_id'];     
+$user_id = $_POST['user_id'];
+$comment = $_POST['comment'];           
+$rating = $_POST['rating'];
 
+?>
 
-echo "hello";
+<li class="box">
+    <?= '<span class="email">' . $product_id. '</span>' ?>
+    <?= '<span class="comment">' . $comment . '</span>' ?>
+</li>
--- index.php
+++ index.php
@@ -1,13 +1,16 @@
 <?php
-
+session_start();
+if (empty($_SESSION["id"]))
+    $_SESSION["id"] = md5(null);
+    
 include_once(__DIR__.'/layouts/header.php');
 
 
 ?>
 
-<form id="info_form">
-    <input type="hidden" name="product_id" id="product_id" value="random-value" >
-    <input type="hidden" name="user_id" id="user_id" value="random-valuesd">
+<form id="info_form" class="form-horizontal">
+    <input type="hidden" name="product_id" id="product_id" value="<?= md5(null); ?>" >
+    <input type="hidden" name="user_id" id="user_id" value="<?= $_SESSION["id"]; ?>">
 
     <textarea name="comment" id="comment_input" cols="30" rows="10"></textarea>
     <input type="hidden" name="rating" id="rating" value=1>
